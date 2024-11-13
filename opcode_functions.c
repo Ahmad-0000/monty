8d8d1c8f7f4fec6123f78d70351b0427d27e6047
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-13 20:39:20 +0000

    New functionalities

--- opcode_functions.c
+++ opcode_functions.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <fcntl.h>
 #include "monty.h"
 
 
@@ -47,7 +46,6 @@ void push(char **linearray, stack_t **rear, unsigned int linenum)
 	*rear = new_node;
 	return;
 error:
-	printf("Here\n");
 	freewarray(linearray);
 	freestack(*rear);
 	exit(EXIT_FAILURE);
@@ -65,83 +63,55 @@ void pall(char **linearray __attribute__((unused)), stack_t **rear, unsigned int
 	}
 }
 
-/*void pint(stack_t **stack_head, unsigned int line_num)
+void pint(char **linearray __attribute__((unused)), stack_t **rear, unsigned int linenum __attribute__((unused)))
 {
-        stack_t *temp;
-        int char_read = 1;
-        char c = '\0';
-
-        if (stack_head == NULL || *stack_head == NULL)
-        {
-            c = skip_after(c, &char_read);
-            errcpi(line_num);
-        }
-        temp = *stack_head;
-        while (temp->next != NULL)
-            temp = temp->next;
-        printf("%d\n", temp->n);
-        c = skip_after(c, &char_read);
+	if (!(*rear))
+	{
+		fprintf(stderr, "L%d: can't pint, stack is empty\n", linenum);
+		freewarray(linearray);
+		freestack(*rear);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*rear)->n);
 }
 
-void pop(stack_t **stack_head, unsigned int line_num)
+void pop(char **linearray __attribute__((unused)), stack_t **rear, unsigned int linenum __attribute__((unused)))
 {
-        stack_t *temp;
-        int char_read = 1;
-        char c = '\0';
-
-        if (stack_head == NULL || *stack_head == NULL)
-        {
-            c = skip_after(c, &char_read);
-            fprintf(stderr , "L%u: can't pop an empty stack\n", line_num);
-            exit(EXIT_FAILURE);
-        }
-        temp = *stack_head;
-        while (temp->next != NULL)
-            temp = temp->next;
-        if (temp->prev == NULL)
-        {
-            free(temp);
-            *stack_head = NULL;
-        }
-        else
-        {
-            temp->prev->next = NULL;
-            free(temp);
-        }
-        skip_after(c, &char_read);
+	if (!(*rear))
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", linenum);
+		freewarray(linearray);
+		exit(EXIT_FAILURE);	
+	}
+	if ((*rear)->prev)
+	{
+		*rear = (*rear)->prev;
+		free((*rear)->next);
+		(*rear)->next = NULL;
+		return;
+	}
+	free(*rear);
+	*rear = NULL;
 }
 
-void swap(stack_t **stack_head, unsigned int line_num)
+void swap(char **linearray __attribute__((unused)), stack_t **rear, unsigned int linenum __attribute__((unused)))
 {
-        int char_read = 1;
-        char c = '\0';
-        stack_t *temp;
-        int nodes_num = 0;
+	int temp;
 
-        if (stack_head == NULL || *stack_head == NULL)
-        {
-            c = skip_after(c, &char_read);
-            errcs(line_num);
-        }
-        temp = *stack_head;
-        while (temp != NULL)
-        {
-            temp = temp->next;
-            nodes_num++;
-        }
-        if (nodes_num < 2)
-        {
-            c = skip_after(c, &char_read);
-            errcs(line_num);
-        }
-        temp = *stack_head;
-        while (temp->next->next != NULL)
-            temp = temp->next;
-        if (nodes_num > 2)
-            temp->prev->next = temp->next;
-        temp->next->prev = temp->prev;
-        temp->next->next = temp;
-        temp->prev = temp->next;
-        temp->next = NULL;
-        skip_after(c, &char_read);
-} */
+	if (!(*rear))
+	{
+		fprintf(stderr, "L%d: can't swap, stack is too short\n", linenum);
+		freewarray(linearray);
+		exit(EXIT_FAILURE);
+	}
+	if (!(*rear)->prev)
+	{
+		fprintf(stderr, "L%d: can't swap, stack is too short\n", linenum);
+		freewarray(linearray);
+		freestack(*rear);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*rear)->prev->n;
+	(*rear)->prev->n = (*rear)->n;
+	(*rear)->n = temp;
+}
---
+++ opcode_functions2.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "monty.h"
+
+void add(char **linearray, stack_t **rear, unsigned int linenum)
+{
+	if (!(*rear))
+	{
+		fprintf(stderr, "L%d: cant't add, stack is too short\n", linenum);
+		freewarray(linearray);
+		exit(EXIT_FAILURE);
+	}
+	if (!(*rear)->prev)
+	{
+		fprintf(stderr, "L%d: cant't add, stack is too short\n", linenum);
+		freestack(*rear);
+		freewarray(linearray);
+		exit(EXIT_FAILURE);
+	}
+	(*rear)->prev->n += (*rear)->n;
+	*rear = (*rear)->prev;
+	free((*rear)->next);
+	(*rear)->next = NULL;
+}
+
+void nop(char **linearray, stack_t **rear, unsigned int linenum)
+{
+	return;
+}
